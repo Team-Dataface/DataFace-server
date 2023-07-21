1b065aae6fee8dccfe0948ac83466a584bcbb5e0
darren-kk <rlawoghks10@gmail.com>
2023-07-21 20:49:31 +0900

    Feat. add verifyToken middleware

---
+++ src/middlewares/verifyToken.js
@@ -0,0 +1,54 @@
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+
+const User = require("../models/User");
+const errors = require("../constants/error");
+const {
+  sign,
+  accessTokenVerify,
+  refreshTokenVerify,
+} = require("../service/jwtUtils");
+
+exports.verifyToken = async function (req, res, next) {
+  try {
+    const { accessToken } = req.cookies;
+
+    if (accessToken) {
+      const authResult = accessTokenVerify(accessToken);
+      const decodedToken = jwt.decode(accessToken);
+
+      const user = await User.findById(decodedToken.id).lean();
+
+      const refreshResult = await refreshTokenVerify(
+        user.refreshToken,
+        decodedToken.id,
+        next,
+      );
+
+      if (!authResult.type && authResult.message === "jwt expired") {
+        if (!refreshResult) {
+          return next(createError(401, errors.NOT_AUTHORIZED.message));
+        }
+
+        const newAccessToken = sign(decodedToken.id);
+
+        res.status(201).cookie("accessToken", newAccessToken, {
+          maxAge: 1000 * 60 * 60,
+          httpOnly: true,
+        });
+
+        return next();
+      }
+
+      req.user = decodedToken.id;
+      return next();
+    }
+
+    return next(createError(401, errors.NOT_AUTHORIZED.message));
+  } catch (error) {
+    error.message = errors.NOT_AUTHORIZED.message;
+    error.status = errors.NOT_AUTHORIZED.status;
+
+    return next(error);
+  }
+};
