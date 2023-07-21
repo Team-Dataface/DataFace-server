a8e5c9b6e5e2905f685e0d53c1219c52d296029a
darren-kk <rlawoghks10@gmail.com>
2023-07-21 20:19:51 +0900

    Feat. add secret key for jwt

--- src/constants/config.js
+++ src/constants/config.js
@@ -4,6 +4,7 @@ const CONFIG = {
   PORT: process.env.PORT,
   MONGODB_URL: process.env.MONGODB_URL,
   CLIENT_URL: process.env.CLIENT_URL,
+  SECRET_KEY: process.env.SECRET_KEY,
 };
 
 module.exports = CONFIG;
