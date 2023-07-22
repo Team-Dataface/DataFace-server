bd0909ff91dbc1ea2e97feede4858a4af19bc122
Leo Kim <leobkkim@gmail.com>
2023-07-22 19:53:37 +0900

    Fix. add default DB name

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -3,7 +3,7 @@ const CONFIG = require("../constants/config");
 
 async function mongooseLoader() {
   try {
-    await mongoose.connect(CONFIG.MONGODB_URL);
+    await mongoose.connect(CONFIG.MONGODB_URL, { dbName: "DataFace" });
     console.log("connected to database");
   } catch (error) {
     console.error(error);
