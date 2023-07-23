e74a3a8162ff79fa988a18a128ec8b6cd16f2cc1
Leo Kim <leobkkim@gmail.com>
2023-07-23 20:51:38 +0900

    Refactor. update routes to better reflect database controller function roles

--- src/loaders/routers.js
+++ src/loaders/routers.js
@@ -1,11 +1,11 @@
 const indexRouter = require("../routes/index");
 const authRouter = require("../routes/auth");
-const userRouter = require("../routes/users");
+const databaseRouter = require("../routes/databases");
 
 async function routerLoader(app) {
   app.use("/", indexRouter);
   app.use("/auth", authRouter);
-  app.use("/users", userRouter);
+  app.use("/users/:userid/databases", databaseRouter);
 }
 
 module.exports = routerLoader;
