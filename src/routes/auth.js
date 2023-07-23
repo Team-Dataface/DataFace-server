79157d3ff13b0ed2401b082ead1ac04024551ebe
darren-kk <rlawoghks10@gmail.com>
2023-07-23 16:37:24 +0900

    Feat. add GET endpoint for user auth Check

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
 const authController = require("../controllers/auth.controller");
+const { verifyToken } = require("../middlewares/verifyToken");
 
 const router = express.Router();
 
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
+router.get("/check", verifyToken, authController.check);
 
 module.exports = router;
