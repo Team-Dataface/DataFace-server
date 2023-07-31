8d9df5337a9aa49d3330659cb9b67f52754953d2
Leo Kim <leobkkim@gmail.com>
2023-07-31 18:01:14 +0900

    Feat. add getRelatedFields controller function

--- src/controllers/relationship.controller.js
+++ src/controllers/relationship.controller.js
@@ -31,3 +31,65 @@ exports.createRelationship = async function (req, res, next) {
     res.status(500).json({ error: "Failed to create database relationship" });
   }
 };
+
+// /:userid/databases/:databaseid/relationships/:relationshipid?primaryFieldValue=_____
+exports.getRelatedFields = async function (req, res, next) {
+  const userId = req.params.userid;
+  const databaseId = req.params.databaseid;
+  const relationshipId = req.params.relationshipid;
+  const { primaryFieldValue } = req.query;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User Not Found" });
+    }
+
+    const database = user.databases.id(databaseId);
+
+    if (!database) {
+      return res.status(404).json({ error: "Database Not Found" });
+    }
+
+    const relationship = database.relationships.id(relationshipId);
+
+    if (!relationship) {
+      return res.status(404).json({ error: "Relationship Not Found" });
+    }
+
+    const { foreignDbId, foreignFieldName, foreignFieldsToDisplay } =
+      relationship;
+
+    const foreignDatabase = user.databases.id(foreignDbId);
+
+    const relatedDocuments = foreignDatabase.documents.filter((document) => {
+      const relatedFields = document.fields.filter(
+        (field) =>
+          field.fieldName === foreignFieldName &&
+          field.fieldValue === primaryFieldValue,
+      );
+
+      return !!relatedFields.length;
+    });
+
+    const displayedDocuments = [];
+
+    relatedDocuments.forEach((document) => {
+      const displayedFields = [];
+
+      document.fields.forEach((field) => {
+        if (foreignFieldsToDisplay.includes(field.fieldName)) {
+          displayedFields.push(field);
+        }
+      });
+
+      displayedDocuments.push({ fields: displayedFields });
+    });
+
+    res.json({ displayedDocuments });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to get related fields" });
+  }
+};
