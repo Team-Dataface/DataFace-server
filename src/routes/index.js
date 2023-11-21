e76108dc7c46b9b2c4795e2cc10f02ab30049369
Youjin-cmd <y.kimiau@gmail.com>
2023-11-21 21:45:14 +0900

    Setting. test libraries

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.status(200).send("success");
+  res.status(200).send({ success: true });
 });
 
 module.exports = router;
